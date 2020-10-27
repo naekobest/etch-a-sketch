f211af0574f33eb61f855c0bab70efdb13a44d70
naekobest <philipbahmann@gmail.com>
2020-10-27 23:41:20 +0100

    Added color function, doesnt work yet

--- js.js
+++ js.js
@@ -1,13 +1,31 @@
 const container = document.querySelector("#container");
 
+// Create a grid with x-rows, x-cols size
 function makeGrid(rows, cols){
 
     for (c = 0; c < (rows * cols); c++){
         container.style.setProperty('--grid-rows', rows);
         container.style.setProperty('--grid-cols', cols);
         let cell = document.createElement("div");
+        cell.addEventListener("mouseenter", function(){
+            const gridCells = document.querySelectorAll("grid-item");
+            gridCells.forEach(e => {
+                e.style.background = "red";
+            })
+        });
         container.appendChild(cell).className = "grid-item";
+        
     }
+}
+
+// Get a random color in hex
+function getRandomColor(){
+    const letters = "0123456789ABCDEF";
+    const color = "#";
+    for (let i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
 makeGrid(16, 16);
