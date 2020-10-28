054f4bbab229f4db9b583e054c9bffc21d4227d6
naekobest <philipbahmann@gmail.com>
2020-10-28 16:39:05 +0100

    Standard etch-a-sketch version working

--- js.js
+++ js.js
@@ -1,4 +1,7 @@
 const container = document.querySelector("#container");
+const colorButton = document.querySelector("#color");
+const resetButton = document.querySelector("#reset");
+const sizeButton = document.querySelector("#size");
 
 // Create a grid with x-rows, x-cols size
 function makeGrid(rows, cols){
@@ -6,26 +9,34 @@ function makeGrid(rows, cols){
     for (c = 0; c < (rows * cols); c++){
         container.style.setProperty('--grid-rows', rows);
         container.style.setProperty('--grid-cols', cols);
-        let cell = document.createElement("div");
-        cell.addEventListener("mouseenter", function(){
-            const gridCells = document.querySelectorAll("grid-item");
-            gridCells.forEach(e => {
-                e.style.background = "red";
-            })
+        const div = document.createElement("div");
+        container.appendChild(div);
+
+        // EventListener for bg-color red
+        div.addEventListener("mouseover", function(){
+            this.style.backgroundColor = "red";
         });
-        container.appendChild(cell).className = "grid-item";
-        
     }
 }
 
-// Get a random color in hex
-function getRandomColor(){
-    const letters = "0123456789ABCDEF";
-    const color = "#";
-    for (let i = 0; i < 6; i++){
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
+// reset the grid to original status
+function resetGrid(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    };
+};
+
+// Eventlistener to reset the grid
+resetButton.addEventListener("click", () => {
+    resetGrid();
+    makeGrid(16, 16);
+});
+
+// Eventlistener to pick random color
+colorButton.addEventListener("click", () => {
+    let color = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+    this.style.backgroundColor = color;
+    console.log(this);
+});
 
 makeGrid(16, 16);
